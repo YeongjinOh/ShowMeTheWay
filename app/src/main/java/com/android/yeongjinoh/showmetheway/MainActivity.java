8c454a47b3ac32fc0cae3628fdec98778f2144ec
YeongjinOh <oyj9109@snu.ac.kr>
2016-07-18 03:28:03 +0900

    onClick method for exit button

--- app/src/main/java/com/android/yeongjinoh/showmetheway/MainActivity.java
+++ app/src/main/java/com/android/yeongjinoh/showmetheway/MainActivity.java
@@ -2,7 +2,9 @@ package com.android.yeongjinoh.showmetheway;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        // initailize main buttons
+        Button buttonMainStart = (Button) findViewById(R.id.btnMainStart);
+        Button buttonMainEnd = (Button) findViewById(R.id.btnMainEnd);
+
+        // set on click on the end button
+        buttonMainEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
     }
 }
