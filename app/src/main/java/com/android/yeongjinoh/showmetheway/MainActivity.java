620318e044427aedda961ee0614a4b7b30f0afc1
YeongjinOh <oyj9109@snu.ac.kr>
2016-07-18 03:01:50 +0900

    apply background image to main page

--- app/src/main/java/com/android/yeongjinoh/showmetheway/MainActivity.java
+++ app/src/main/java/com/android/yeongjinoh/showmetheway/MainActivity.java
@@ -2,12 +2,15 @@ package com.android.yeongjinoh.showmetheway;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
     }
 }
