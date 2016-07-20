c2676b7b9d27cde85806293a8de435995559dc2e
YeongjinOh <oyj9109@snu.ac.kr>
2016-07-20 14:28:41 +0900

    set menu button functionality on scorehistory page

--- app/src/main/java/com/android/yeongjinoh/showmetheway/ScoreHistoryActivity.java
+++ app/src/main/java/com/android/yeongjinoh/showmetheway/ScoreHistoryActivity.java
@@ -2,6 +2,8 @@ package com.android.yeongjinoh.showmetheway;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by yeongjinoh on 2016-07-20.
@@ -11,5 +13,13 @@ public class ScoreHistoryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scorehistory);
+
+        Button buttonScoreMenu = (Button) findViewById(R.id.btnScoreMenu);
+        buttonScoreMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
