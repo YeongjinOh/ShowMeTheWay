7aad6da4109b35458ce196357625e6dcd9804927
YeongjinOh <oyj9109@snu.ac.kr>
2016-07-22 01:39:06 +0900

    save user information in SharedPreferences

--- app/src/main/java/com/android/yeongjinoh/showmetheway/AccountSetActivity.java
+++ app/src/main/java/com/android/yeongjinoh/showmetheway/AccountSetActivity.java
@@ -1,9 +1,11 @@
 package com.android.yeongjinoh.showmetheway;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by yeongjinoh on 2016-07-21.
@@ -20,6 +22,18 @@ public class AccountSetActivity extends Activity {
             @Override
             public void onClick(View view) {
 
+                // get user inputs from EditText objects
+                EditText textUsername = (EditText) findViewById(R.id.usernameEntry);
+                EditText textEmailaddress = (EditText) findViewById(R.id.emailaddressEntry);
+                String username = textUsername.getText().toString();
+                String emailaddress = textEmailaddress.getText().toString();
+
+                // save them into SharedPreferences
+                SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putString("username", username);
+                editor.putString("emailaddress",emailaddress);
+                editor.commit();
             }
         });
         buttonCancel.setOnClickListener(new View.OnClickListener() {
