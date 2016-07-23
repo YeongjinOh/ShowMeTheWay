569ce91b3295dae31f2797868566f09ce0527350
YeongjinOh <oyj9109@snu.ac.kr>
2016-07-24 06:29:24 +0900

    take my best score

--- app/src/main/java/com/android/yeongjinoh/showmetheway/RankActivity.java
+++ app/src/main/java/com/android/yeongjinoh/showmetheway/RankActivity.java
@@ -1,18 +1,43 @@
 package com.android.yeongjinoh.showmetheway;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by yeongjinoh on 2016-07-22.
  */
 public class RankActivity extends Activity {
+
+    private DatabaseHelper dbHelper;
+    private SQLiteDatabase db;
+    private static String TABLE_NAME = "score";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rank);
+
+        // get my best score
+
+        // read database
+        dbHelper = new DatabaseHelper(this);
+        db = dbHelper.getWritableDatabase();
+        String SQL = "select score "
+                + " from " + TABLE_NAME
+                + " order by score desc";
+        Cursor c1 = db.rawQuery(SQL, null);
+        c1.moveToNext();
+        int score = c1.getInt(0);
+
+        TextView scoreView = (TextView) findViewById(R.id.scoreViewRank);
+        scoreView.setText("My score : " + Integer.toString(score));
 
         Button buttonBack = (Button) findViewById(R.id.btnRankBack);
         buttonBack.setOnClickListener(new View.OnClickListener() {
